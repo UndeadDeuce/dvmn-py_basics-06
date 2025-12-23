c68ea437da1cc5260a5129421db82e8a0c872f64
UndeadDeuce <notima.ak@gmail.com>
2025-12-23 12:47:43 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
-def has_digit(password):
+SCORE_PER_CHECK = 2
+MIN_LENGTH = 12
+
+
+def has_digit(password: str):
     return any(ch.isdigit() for ch in password)
 
 
-def is_very_long(password):
-    return len(password) > 12
+def is_very_long(password: str):
+    return len(password) > MIN_LENGTH
 
 
-def has_upper_letters(password):
+def has_upper_letters(password: str):
     return any(ch.isupper() for ch in password)
 
 
-def has_lower_letters(password):
+def has_lower_letters(password: str):
     return any(ch.islower() for ch in password)
 
 
-def has_symbols(password):
-    special_symbols = ['!', '@', '#', '$', '%', '&']
-    return any(ch in special_symbols for ch in password)
+def has_symbols(password: str):
+    return any(not ch.isdigit() and not ch.isalpha() for ch in password)
 
 
-my_pass = input()
-score = 0
+def main() -> None:
+    my_pass = input()
+    score = 0
 
-checks = [
-    is_very_long,
-    has_digit,
-    has_upper_letters,
-    has_lower_letters,
-    has_symbols,
-]
+    checks = [
+        is_very_long,
+        has_digit,
+        has_upper_letters,
+        has_lower_letters,
+        has_symbols,
+    ]
 
-for check in checks:
-    if check(my_pass):
-        score += 2
+    for check in checks:
+        if check(my_pass):
+            score += SCORE_PER_CHECK
 
-print('Рейтинг пароля:', score)
+    print("Рейтинг пароля: {score}".format(score=score))
+
+
+if __name__ == "__main__":
+    main()
